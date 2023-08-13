f881b49dad4e888cbb345950a11748ecd3beb5c0
nekkunjpilaniarvikasoft <132471789+nekkunjpilaniarvikasoft@users.noreply.github.com>
2023-08-13 00:34:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3001;
+const path =require('path')
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+const __dirname = path.resolve();
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -42,6 +44,12 @@ app.post("/create", (req, res) => {
     .catch((err) => console.log(err));
 });
 
+
+app.use(express.static("./client/build"));
+app.get("*",(req,res)=>{
+  res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+});
 app.listen(port, () => {
   console.log(`Server is running on post ${port}`);
 });
+
